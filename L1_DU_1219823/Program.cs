8aa05335c65799962d880be367a635a30d44507e
Diego Urbina <Diego Urbina@DESKTOP-44BRJDT>
2023-02-18 11:23:17 -0600

    Agregar archivos de proyecto.

---
+++ L1_DU_1219823/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su nombre: ");
+            string Nombre = Console.ReadLine();
+        Console.WriteLine("Hello, World! soy " + Nombre );
+        Console.ReadKey();
+        /* COMENTARIOS */
+        Console.Write("Hola mundo");
+        Console.Write("Soy " + Nombre);
+        Console.ReadKey();
+    }
+}
